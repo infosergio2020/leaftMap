57a05aa1a0c90ef79a85e88b038dc961c67d1bbf
lau-yt <laura.y.lp96@gmail.com>
2022-02-25 14:30:11 -0300

    Mostrar todas las zonas agregada

--- js/map.js
+++ js/map.js
@@ -179,14 +179,14 @@ export function filterMarker(opcionName){
     console.log('filterMarker with option: '+opcionName);
     let elements = document.querySelectorAll(".leaflet-marker-icon");
     for (var i = 0; i < elements.length; ++i) {
-     if (opcionName != elements[i].dataset.zona){ //Los marcadores que no sean de la zona se esconden
-        elements[i].setAttribute("tabindex", '-1');
-        elements[i].style.visibility='hidden';
-     }
-     else{ //marcadores que SI PERTENECEN A LA ZONA se vuelven a mostrar
+     if ((opcionName == elements[i].dataset.zona)||(opcionName == 'Todas las zonas')){ //marcadores que SI PERTENECEN A LA ZONA se vuelven a mostrar
         console.log('mostrar!');
         elements[i].setAttribute("tabindex", i+1); // probar bien lo de tabindex
         elements[i].style.visibility='visible';
+     }
+     else{ //Los marcadores que NO sean de la ZONA se esconden
+        elements[i].setAttribute("tabindex", '-1');
+        elements[i].style.visibility='hidden';
      }
     }     
 }
